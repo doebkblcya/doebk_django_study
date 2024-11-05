d01168b3cdc44f54c4eca6e3681435ee47afff28
doebkblcya <2738103877@qq.com>
2024-11-05 15:18:53 +0800

    2024.11.5 django

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
+
 # Create your views here.
 
 def index(request):
@@ -16,3 +18,15 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
